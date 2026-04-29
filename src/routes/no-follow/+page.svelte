8ea1ef51d984aec767fca13afc179f83876b7d1a
JaunPaul <stevenson@kreonovo.com>
2026-04-29 09:38:42 +0200

    created email hero component

---
+++ src/routes/no-follow/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import HeroLargeEmailCta from '$lib/components/page-sections/HeroLargeEmailCta.svelte';
+</script>
+
+<HeroLargeEmailCta
+	props={{
+		heading: 'Ignite the stage',
+		labelText: 'Ready for transformation?',
+		mailto: 'mailto:speaker@christophholz.com'
+	}}
+></HeroLargeEmailCta>
